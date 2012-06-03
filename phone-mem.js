7d79e6d04b5de7dc729480cb8f42bd6f3a429a57
Amir Sharif <budgeneration@gmail.com>
2012-06-03 12:33:59 -0400

    First attempt, doesn't get enough combinations... tougher problem than I thought

--- phone-mem.js
+++ phone-mem.js
@@ -11,6 +11,38 @@ var o = {
 	'9': ['W', 'X', 'Y', 'Z']
 };
 
+function convert_to_letters(numbers) {
+	var letters = "";
+	for( var i = 0; i<numbers.length; i++ ){
+		letters += o[ numbers[i] ][0];
+	}
+	return letters;
+}
+
+function process(phone_number) {
+	var words = [];
+	var nWords = 0;
+	
+	function process_helper(num, index) {
+		var temp_num = num[index];
+		var states_for_num = o[ temp_num ];
+		
+		for( var i = 0; i<states_for_num.length; i++ ) {
+			var beforeWord = convert_to_letters(num.substring(0, index));
+			var afterWord = convert_to_letters(num.substring(index+1));
+			var letter = states_for_num[i];
+			words[nWords] = beforeWord + letter + afterWord;
+			nWords++;
+		}
+	}
+	
+	for( var i = 0; i<phone_number.length; i++ ){
+		process_helper(phone_number, i);
+	}
+	
+	output( words );
+}
+
 function run(event) {
 	if( event.keyCode == 13 ) {
 		// Sanitize everything except numbers
@@ -18,20 +50,16 @@ function run(event) {
 		var phone_number = document.getElementById("input").value;
 		phone_number = phone_number.replace(r, "");
 		document.getElementById("input").value = "";
+		process(phone_number);
 	}
-	
-	var words = [];
-	var nWords = 0;
-	
-	output( words );
 }
 
 function output (lines) {
-	var results = document.getElementById("results")
-	results.innerHTML = lines.join(',').replace(',', '\n');
+	var results = document.getElementById("results");
+	results.innerHTML = lines.join(',').replace(/\,/g, '\n');
 }
 
 function init() {
-	var input = document.getElementById("input")
-	input.onkeypress = run
+	var input = document.getElementById("input");
+	input.onkeypress = run;
 }
