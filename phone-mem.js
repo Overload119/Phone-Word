d31be6f0347ee41b3f05654fca16836baba3c3b5
Overload119 <budgeneration@gmail.com>
2013-02-25 12:57:33 -0500

    Update phone-mem.js

--- phone-mem.js
+++ phone-mem.js
@@ -11,14 +11,6 @@ var o = {
 	'9': ['W', 'X', 'Y', 'Z']
 };
 
-function convert_to_letters(numbers) {
-	var letters = "";
-	for( var i = 0; i<numbers.length; i++ ){
-		letters += o[ numbers[i] ][0];
-	}
-	return letters;
-}
-
 // The number of combinations it is supposed to find is:
 // ([ size of array for number at index 1 ] choose 1)([ size of array for number at index 2 ] choose 1) ... ([ size of array for number at index i ] choose 1) for 0 < i < inf.
 // IE. For number 929 we have
@@ -36,69 +28,29 @@ function get_letters(num, statenum) {
 	return word;
 }
 
-
-function process(phone_number) {
-	var words = [];
-	var nWords = 0;
-	var initial_state = [];
-	var max_state = [];
-	var possible_states = [];
-	var ps_c = 0;
-	for( var i = 0; i<phone_number.length; i++ ){
-		initial_state[i] = 0;
-		max_state[i] = o[phone_number[i]].length;
-	}
-	//console.log("Initial State: "+initial_state);
-	//console.log("Max State: "+max_state);
-	
-	// Can be optimized, currently a recursive solution
-	function process_helper(num_state, index) {
-		if( num_state[index+1] == null ) {
-			// Get all the states
-			var states = [];
-			for( var i = 0; i<max_state[index]; i++){
-				var state = new Array();
-				// Copy the num_state
-				for( var k = 0; k<initial_state.length; k++){
-					state[k] = initial_state[k];
-				}
-				
-				state[index] = i;
-				//console.log("-> Pushing " + state);
-				states.push(state);
-			}
-			//console.log("--> Returning: " + states);
-			return states;
-		} else {
-			var prev_states = process_helper(num_state, index+1);
-			var states = [];
-			for( var j = 0; j<prev_states.length; j++){
-				var state = new Array();
-				for( var k = 0; k<prev_states[j].length; k++){
-					state[k] = prev_states[j][k];
-				}
-				for( var i = 0; i<max_state[index]; i++){
-					var state_copy = [];
-					for( var k = 0; k<prev_states[j].length; k++){
-						state_copy[k] = state[k];
-					}
-					state_copy[index] = i;
-					states.push(state_copy);
-					//console.log("-> Pushing " + state_copy);
-				}
-			}
-			//console.log("--> Returning: " + states + " ("+states.length+") ");
-			return states;
-		}
-	}
-	var allStates = process_helper(initial_state, 0);
-	for( var i = 0, c=allStates.length; i<c; i++ ){
-		words[nWords] = get_letters(phone_number, allStates[i]);
-		nWords++;
-	}
-	
-	//console.log("Final: "+allStates);
-	output( words );
+// @number (Int) -> @Array
+function process(number) {
+  number = number.toString();
+  var numberArray = [];
+  for( var i = 0; i < number.length; i++ ) {
+    numberArray.push(NUMBER_LETTER_MAP[number[i]])
+  }
+  var result = [];
+  function process_helper(array, set) {
+    if( array.length === 1 ) {
+      for(var i = 0; i < array[0].length; i++) {
+        result.push( set.concat( [ array[0][i] ] ) );
+      }
+    } else {
+      var letterSet = array[0];
+      var subset = array.slice(1);
+      for(var i = 0; i < letterSet.length; i++) {
+        helper(subset, set.concat([letterSet[i]]));
+      }
+    }
+  }
+  process_helper(numberArray, []);
+  return result;
 }
 
 function run(event) {
